9915754a71fb284c8a6b9f3b563c92695309b5fc
Siddhant Singh <technosys.sidd@gmail.com>
2024-03-08 16:32:52 +0530

    added the endpoint to get details regarding a specific scheduler

--- backend/src/Routers/schedule.js
+++ backend/src/Routers/schedule.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import auth from '../middleware/auth.js';
 import scheduleModel from '../Models/scheduleModel.js';
+import accountModel from '../Models/accountModel.js';
+import meetingModel from '../Models/meetingModal.js';
 
 const Router = new express.Router();
 
@@ -12,6 +14,27 @@ Router.get('/schedule/endpoint/allSchedulers', auth, async (req, res) => {
         }
         res.status(200).send({schedulers})
     } catch (error) {
+        res.status(500).send({error: 'Server Error'})
+    }
+})
+
+Router.get('/schedule/endpoint/:name', async (req, res) => {
+    try{
+        const scheduler = await scheduleModel.findOne({schedulerName: req.params.name})
+        if(!scheduler){
+            return res.status(404).send({noScheduler: 'Scheduler with this name is not found'})
+        }
+        const account = await accountModel.findById(scheduler.userId)
+        if(!account){
+            return res.status(404).send({noScheduler: 'Scheduler with this name is not found'})
+        }
+        const meetings = await meetingModel.find({email: account.email})
+        if(meetings.length === 0){
+            return res.status(200).send({...scheduler, email: account.email, bookedTime: []})
+        }
+        const bookedTime = meetings.map((meeting) => meeting.time)
+        res.status(200).send({...scheduler, email: account.email, bookedTime})
+    } catch(error){
         res.status(500).send({error: 'Server Error'})
     }
 })

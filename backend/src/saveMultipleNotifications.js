ccace891569525ec099ebbb2516c72e2d53d60ea
Siddhant Singh <technosys.sidd@gmail.com>
2024-03-09 13:30:41 +0530

    created the saveMultipleNotifications util

---
+++ backend/src/saveMultipleNotifications.js
@@ -0,0 +1,11 @@
+import accountModel from './Models/accountModel';
+import moment from 'moment-timezone';
+
+const saveMultipleNotifications = async (notification, criteria, emails) => {
+    const time = moment().tz("America/Los_Angeles").format();
+    const data = {notification,criteria,time};
+    await accountModel.updateMany({ email: { $in: emails } }, { $push: { notifications: data } });
+}
+
+
+export default saveMultipleNotifications;

1a36cf0e14158079bcdbbd7e855ca5f4fe5927b9
Siddhant Singh <technosys.sidd@gmail.com>
2024-03-09 16:32:28 +0530

    created the endpoint to redirect user with to the generated auth url

--- backend/src/Routers/google-auth.js
+++ backend/src/Routers/google-auth.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { google } from 'googleapis';
-
+import auth from '../middleware/auth.js'; 
 
 const Router = express.Router();
 
@@ -11,5 +11,16 @@ const REDIRECT_URI = process.env.NODE_ENV === 'production' ? 'https://meetzflow.
 const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 
 const SCOPES = ['https://www.googleapis.com/auth/calendar.events'];
+
+Router.get('/auth/google', auth, (req, res) => {
+    if(req.user.googleAuthorizationCode){
+      return res.status(400).send('Your Google Calendar Account is already connected')
+    }
+    const url = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: SCOPES,
+    });
+    res.redirect(url);
+});
 
 export const AuthRouter = Router;

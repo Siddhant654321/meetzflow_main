235b0dd6d28fd4c1b69d331666039d052870bf92
Siddhant Singh <technosys.sidd@gmail.com>
2024-03-11 23:42:51 +0530

    added the socket io configuration to handle the users real-time chats

--- backend/index.js
+++ backend/index.js
@@ -11,11 +11,20 @@ import {AuthRouter} from './src/Routers/google-auth.js';
 import teamRouter from './src/Routers/team.js';
 import chatRouter from './src/Routers/chat.js';
 import emailModify from './src/middleware/emailModify.js';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const corsOptions = {
     origin: 'https://meetzflow.com', 
     credentials: true
 };
+const server = createServer(app);
+const io = new Server(server, {cors: corsOptions});
 app.options('*', cors(corsOptions));
 app.use(cors(corsOptions))
 app.use(cookieParser())
@@ -29,13 +38,75 @@ app.use(contactRouter)
 app.use(AuthRouter)
 app.use(teamRouter)
 app.use(chatRouter)
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
     res.send('Welcome to MeetzFlow')
 })
 
+let onlineUsers = []
+
+io.on('connection', async (socket) => {
+    let team;
+    let accountName;
+    const {teamName, userEmail} = socket.handshake.query
+    try{
+      const user = (await accountModel.findOne({email: userEmail}));
+      const {_id} = user;
+      accountName = user.name;
+      team = await teamModel.findOne({team: teamName,
+        $or: [
+            { "admin.email": userEmail },
+            { "members.email": userEmail }
+        ]
+      })
+      if(!team){
+        throw new Error('Team not found')
+      }
+      socket.join(String(team._id));
+      onlineUsers.push({email: userEmail, teamId: String(team._id)})
+    } catch (error) {
+      socket.emit('error', 'Team Not Found')
+      socket.disconnect()
+      return; 
+    }
+  
+    const emails = onlineUsers.filter(value => {
+      if(value.teamId === String(team._id)){
+        return value.email
+      }
+    })
+    
+    io.to(String(team._id)).emit('newOnline', emails);
+  
+    socket.on('sendImage', (data) => {
+      const images = {image: data, from: accountName}
+      socket.to(String(team._id)).emit('receiveImage', images);
+    });
+    
+    socket.on('sendMessage', data => {
+      const message = {message: data, from: accountName}
+      socket.to(String(team._id)).emit('receiveMessage', message)
+    })
+  
+    socket.on('sendMeetingMessage', data => {
+      const message = {...data, from: accountName}
+      socket.to(String(team._id)).emit('receiveMeetingMessage', message)
+    })
+  
+    socket.on('disconnect', () => {
+      onlineUsers = onlineUsers.filter(user => user.email !== userEmail);
+      const emails = onlineUsers.filter(value => {
+        if(value.teamId === String(team._id)){
+          return value.email
+        }
+      })
+      io.to(String(team._id)).emit('newOffline', emails);
+    })
+});
+
 const port = process.env.PORT || 3000
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Server is started on port', process.env.PORT || 3000)
 })

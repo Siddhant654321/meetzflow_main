1633e293eb19ba4b88c67deb7c17233d3042a3d6
Siddhant Singh <technosys.sidd@gmail.com>
2024-03-11 15:49:23 +0530

    created the storage configurations and upload middleware in multer

--- backend/src/Routers/chat.js
+++ backend/src/Routers/chat.js
@@ -2,6 +2,9 @@ import express from 'express'
 import auth from '../middleware/auth.js'
 import chatModel from '../Models/chatModel.js'
 import getChat from '../middleware/getChat.js'
+import multer from 'multer'
+import path from 'path'
+import crypto from 'crypto'
 
 const Router = new express.Router();
 
@@ -14,5 +17,29 @@ Router.post('/chat/endpoint/newMessage', auth, getChat, async (req, res) => {
         res.status(500).send({error: 'Server Error'})
     }
 })
+
+const storage = multer.diskStorage({
+    destination: `./chatImages/`,
+    filename: function (req, file, cb) {
+        crypto.pseudoRandomBytes(16, function (err, raw) {
+          if (err) return cb(err);
+          cb(null, raw.toString('hex') + path.extname(file.originalname));
+        });
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+      if (file.mimetype.startsWith('image/')) {
+        cb(null, true);
+      } else {
+        cb(null, false);
+      }
+    },
+    limits: {
+      files: 10
+    }
+});
 
 export default Router;

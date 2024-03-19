3f95e5c870b1e2e9f1ed6c81093cd06fbf2db405
Siddhant Singh <technosys.sidd@gmail.com>
2024-03-19 16:31:00 +0530

    created a list of all time zones

---
+++ frontend/src/utilities/timeZoneList.js
@@ -0,0 +1,33 @@
+const timeZoneList = [
+    "America/New_York",         // Eastern Time (US & Canada)
+    "America/Chicago",          // Central Time (US & Canada)
+    "America/Denver",           // Mountain Time (US & Canada)
+    "America/Los_Angeles",      // Pacific Time (US & Canada)
+    "America/Anchorage",        // Alaska (US & Canada)
+    "America/Honolulu",         // Hawaii
+    "America/Toronto",          // Eastern Time (Canada)
+    "America/Mexico_City",      // Central Time (Mexico)
+    "America/Buenos_Aires",     // Argentina
+    "Europe/London",            // London (UK)
+    "Europe/Paris",             // Paris (France)
+    "Europe/Berlin",            // Berlin (Germany)
+    "Europe/Moscow",            // Moscow (Russia)
+    "Asia/Tokyo",               // Tokyo (Japan)
+    "Asia/Shanghai",            // Shanghai (China)
+    "Asia/Dubai",               // Dubai (UAE)
+    "Asia/Kolkata",             // Kolkata (India)
+    "Asia/Singapore",           // Singapore
+    "Asia/Seoul",               // Seoul (South Korea)
+    "Australia/Sydney",         // Sydney (Australia)
+    "Australia/Perth",          // Perth (Australia)
+    "Africa/Johannesburg",      // Johannesburg (South Africa)
+    "Africa/Cairo",             // Cairo (Egypt)
+    "Atlantic/Azores",          // Azores (Portugal)
+    "America/Sao_Paulo",        // São Paulo (Brazil)
+    "Asia/Bangkok",             // Bangkok (Thailand)
+    "Asia/Manila",              // Manila (Philippines)
+    "Europe/Istanbul",          // Istanbul (Turkey)
+    "Pacific/Auckland"          // Auckland (New Zealand)
+];
+
+export default timeZoneList;

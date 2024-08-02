2ce769bc1afb2d5f26825f7c18d462f025c5a866
Siddhant Singh <technosys.sidd@gmail.com>
2024-08-02 21:18:00 +0530

    created the new navbar

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,11 +1,22 @@
 import { Outlet } from "react-router-dom";
+import "./styles/navbar.css";
+import nav_logo from "./assets/nav_logo.svg";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav>Navbar</nav>
+    <main className="m-container">
+      <nav className="m-nav">
+        <img src={nav_logo} alt="MeetzFlow" />
+        <ul className="m-nav-links">
+          <li>HOME</li>
+          <li>FEATURES</li>
+          <li>TESTIMONIALS</li>
+          <li>CONTACT</li>
+        </ul>
+        <button className="m-get-started-btn">GET STARTED </button>
+      </nav>
       <Outlet />
-    </div>
+    </main>
   );
 };
 export default Navbar;

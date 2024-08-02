bc03cc7bd932b34f7402901cd16e443769a5f6b7
Siddhant Singh <technosys.sidd@gmail.com>
2024-08-02 21:36:50 +0530

    created the hero section

--- frontend/src/Homepage.jsx
+++ frontend/src/Homepage.jsx
@@ -1,4 +1,22 @@
+import "./styles/homepage.css";
+import right_arrow from "./assets/right-arrow.svg";
+import hero_section_img from "./assets/hero_section_img.png";
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <div className="m-hero-section">
+        <h1>Connect Seamlessly</h1>
+        <h3 className="m-subheading">
+          With your Clients and Team members in a single dashboard
+        </h3>
+        <button className="m-get-started-btn m-with-icon-btn">
+          START FOR FREE
+          <img src={right_arrow} alt="" />
+        </button>
+        <img src={hero_section_img} alt="" className="m-hero-section-img" />
+      </div>
+    </div>
+  );
 };
 export default Homepage;

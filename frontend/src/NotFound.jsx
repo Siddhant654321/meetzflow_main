3a1c2e16960976433860fdd3e629ee66b5e8b58a
Siddhant Singh <technosys.sidd@gmail.com>
2024-08-22 16:03:14 +0530

    edited the not found page

--- frontend/src/NotFound.jsx
+++ frontend/src/NotFound.jsx
@@ -1,13 +1,26 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import FlipLink from "./Components/FlipLink";
 
 export default function NotFound() {
-    const navigate = useNavigate();
-    return (
-        <div className='ve-background'>
-            <h3>The page you are looking for does not exist!</h3>
-            {!localStorage.getItem('loggedIn') ? 
-            <button id="ve-btn" onClick={() => navigate('/')}>Go To Home</button> : 
-            <button id="ve-btn" onClick={() => navigate('/app')}>Go To Dashboard</button>}
-        </div>
-    )
+  const navigate = useNavigate();
+  return (
+    <div className="ve-background">
+      <h3 className="m-subheading" style={{ fontSize: 32 }}>
+        The page you are looking for does not exist!
+      </h3>
+      {!localStorage.getItem("loggedIn") ? (
+        <FlipLink
+          className="m-get-started-btn"
+          buttonText="Go To Home"
+          onClick={() => navigate("/")}
+        />
+      ) : (
+        <FlipLink
+          className="m-get-started-btn"
+          buttonText="Go To Dashboard"
+          onClick={() => navigate("/app")}
+        />
+      )}
+    </div>
+  );
 }
